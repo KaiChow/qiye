02974c4feb29829f4bd211953660df3221716f3c
KaiChow <1065438834@qq.com>
2018-11-14 14:04:32 +0800

    2018-11-14-new

---
+++ application/admin/common/Base.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kevin
+ * Date: 2018/11/14
+ * Time: 12:24
+ */
+namespace app\admin\common;
+use think\Controller;
+
+class Base extends Controller {
+
+}
